8792ae269c9a39f5829525100fb8ca2f7009e17f
FG-TUM <f.gratl@tum.de>
2018-10-19 09:33:23 +0200

    empty doxygen module

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,33 +1,5 @@
 # make doc_doxygen optional if someone does not have / like doxygen
-option(BUILD_TARGET_DOC "Create \"make doc_doxygen\" target (requires Doxygen)" ON)
 
-# do nothing if nothing should be done
-if (NOT BUILD_TARGET_DOC)
-    return()
-endif ()
+# TODO: create CMake build option for the target.
 
-# check if Doxygen is installed
-find_package(Doxygen
-        COMPONENTS dot
-        OPTIONAL_COMPONENTS mscgen dia)
-if (DOXYGEN_FOUND)
-    # set input and output files
-    set(DOXY_CONF_DIR docs)
-    set(DOXYGEN_IN ${DOXY_CONF_DIR}/Doxyfile.in)
-    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-    set(DOXY_MAIN_PAGE README.md)
-
-    # request to configure the file
-    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
-    message(STATUS "Doxygen configured")
-
-    # note the option ALL which allows to build the docs together with the application
-    add_custom_target(doc_doxygen
-            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "Generating API documentation with Doxygen"
-            VERBATIM )
-else ()
-    message(WARNING "Doxygen needs to be installed to generate the doxygen documentation, you might also have to install dot (graphviz)")
-    set(BUILD_TARGET_DOC OFF CACHE BOOL "" FORCE)
-endif ()
+# TODO: Add a custom target for building the documentation.
