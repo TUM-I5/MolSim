920b36e29825090472f1d60dd5b7b1994d346db4
FG-TUM <f.gratl@tum.de>
2020-12-05 16:36:24 +0100

    added dimension option to maxwell boltzmann

--- src/utils/MaxwellBoltzmannDistribution.h
+++ src/utils/MaxwellBoltzmannDistribution.h
@@ -13,10 +13,11 @@
 /**
  * Generate a random velocity vector according to the Maxwell-Boltzmann distribution, with a given average velocity.
  *
- * @averageVelocity The average velocity of the brownian motion for the system.
+ * @param averageVelocity The average velocity of the brownian motion for the system.
+ * @param dimensions Number of dimensions for which the velocity vector shall be generated. Set this to 2 or 3.
  * @return Array containing the generated velocity vector.
  */
-std::array<double, 3> maxwellBoltzmannDistributedVelocity(double averageVelocity) {
+std::array<double, 3> maxwellBoltzmannDistributedVelocity(double averageVelocity, size_t dimensions) {
   // we use a constant seed for repeatability.
   // random engine needs static lifetime otherwise it would be recreated for every call.
   static std::default_random_engine randomEngine(42);
@@ -25,8 +26,8 @@ std::array<double, 3> maxwellBoltzmannDistributedVelocity(double averageVelocity
   // the velocity change is maxwell boltzmann distributed
   std::normal_distribution<double> normalDistribution{0, 1};
   std::array<double, 3> randomVelocity{};
-  for (double &v : randomVelocity) {
-    v = averageVelocity * normalDistribution(randomEngine);
+  for (size_t i = 0; i < dimensions; ++i) {
+    randomVelocity[i] = averageVelocity * normalDistribution(randomEngine);
   }
   return randomVelocity;
 }
