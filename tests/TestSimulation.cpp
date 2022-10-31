421366491601799d8cf7a15f71c2ab9781b24098
Alex Hocks <alex.hocks@tum.de>
2022-10-31 21:31:58 +0100

    fix doctest and updated README.MD

--- tests/TestSimulation.cpp
+++ tests/TestSimulation.cpp
@@ -3,18 +3,30 @@
 //
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
+#include "doctest.h"
 static_assert(__cplusplus >= 202002L);
+
+#include <Eigen>
 
 #include "Simulation.h"
 #include "ParticleContainer.h"
+#include "Particle.h"
+
+TEST_CASE("forceBetw2Particles") {
+    Particle p1 { {0, 0, 0}, {1, 0, 0}, 1.0, 0 };
+    Particle p2 { {1, 0, 0}, {1, 0, 0}, 1.0, 0 };
+    const auto f1 {p1.getF()};
+    const auto f2 {p2.getF()};
+
+    REQUIRE((f1 == Eigen::Vector3d {0, 0, 0}));
+    REQUIRE((f2 == Eigen::Vector3d {0, 0, 0}));
+
+    sim::forceBetw2Particles(p1, p2);
+    CHECK((p1.getF() == -p2.getF()));
+}
 
 TEST_CASE("calculateF") {
-    sim::particleContainer = ParticleContainer();
-    REQUIRE((sim::particleContainer.size() == 0));
-    SUBCASE("test"){
-        CHECK((sim::particleContainer.getParticles().size() == 0));
-    }
+
 }
 
 TEST_CASE("calculateX") {
