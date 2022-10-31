787ee1bcdd96ec1b2a4542d355f6cc353cce0a5f
Alex Hocks <alex.hocks@tum.de>
2022-10-31 09:17:37 +0100

    WIP add doctest for unit testing

---
+++ tests/TestSimulation.cpp
@@ -0,0 +1,20 @@
+//
+// Created by alex on 30.10.2022.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest.h>
+static_assert(__cplusplus >= 202002L);
+
+TEST_CASE("calculateF") {
+
+}
+
+TEST_CASE("calculateX") {
+
+}
+
+TEST_CASE("calculateY") {
+
+}
