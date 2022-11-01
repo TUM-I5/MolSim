3c7ca4f721d1ee18e978c730c2146ae5f1473a5c
Al3xand1a <98582325+Al3xand1a@users.noreply.github.com>
2022-11-01 20:18:18 +0100

    adapted cmake for doxygen output

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,19 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+find_package(Doxygen)
 
-# TODO: create CMake build option for the target.
+# make doc_doxygen optional if someone does not have / like doxygen –– Default is ON
+option(BUILD_DOC "Build documentation (requires Doxygen)" OFF)
 
-# TODO: Add a custom target for building the documentation.
+if(BUILD_DOC)
+    if(NOT DOXYGEN_FOUND)
+        message("Missing doxygen, required to generate the documentation")
+    endif()
+
+    # TODO: create CMake build option for the target.
+    # TODO: Add a custom target for building the documentation.
+    add_custom_target( 
+        doc_doxygen
+        COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in 
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+endif(BUILD_DOC)
