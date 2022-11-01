8c3f5708d13cc001e2e7c85dc8cdb0c60af19337
ge65hol <a.marquardt@tum.de>
2022-11-01 20:58:20 +0100

    restore cmake for doxygen

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,17 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+find_package(Doxygen)
+# make doc_doxygen optional if someone does not have / like doxygen –– Default is ON
+option(BUILD_DOC "Build documentation (requires Doxygen)" OFF)
+if(BUILD_DOC)
+    if(NOT DOXYGEN_FOUND)
+        message("Missing doxygen, required to generate the documentation")
+    endif()
+    # TODO: create CMake build option for the target.
+     # TODO: Add a custom target for building the documentation.
+     add_custom_target( 
+         doc_doxygen
+         COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in 
+         COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile 
+         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+     )
 
-# TODO: create CMake build option for the target.
-
-# TODO: Add a custom target for building the documentation.
+endif(BUILD_DOC)
