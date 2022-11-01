a19fae96f2d88912b09d9320162492fdef95d8a7
Alexandra <a.marquardt@tum.de>
2022-11-01 20:47:31 +0100

    Revert "adapted cmake for doxygen output"
    This reverts commit 3c7ca4f721d1ee18e978c730c2146ae5f1473a5c.

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,19 +1,5 @@
-find_package(Doxygen)
+# make doc_doxygen optional if someone does not have / like doxygen
 
-# make doc_doxygen optional if someone does not have / like doxygen –– Default is ON
-option(BUILD_DOC "Build documentation (requires Doxygen)" OFF)
+# TODO: create CMake build option for the target.
 
-if(BUILD_DOC)
-    if(NOT DOXYGEN_FOUND)
-        message("Missing doxygen, required to generate the documentation")
-    endif()
-
-    # TODO: create CMake build option for the target.
-    # TODO: Add a custom target for building the documentation.
-    add_custom_target( 
-        doc_doxygen
-        COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in 
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
-
-endif(BUILD_DOC)
+# TODO: Add a custom target for building the documentation.
