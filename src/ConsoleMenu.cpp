c2773acf4589766799ae8f1fd5fd5060211d8875
ge65hol <a.marquardt@tum.de>
2022-11-28 17:41:49 +0100

    command line options for sigma and epsilon

--- src/ConsoleMenu.cpp
+++ src/ConsoleMenu.cpp
@@ -69,6 +69,16 @@ const void ConsoleMenu::openMenu()
                 _programParameters->setDeltaT(std::__cxx11::stod(parameter));
                 std::cout << "MolSim Group G > Delta time was set to " << _programParameters->getDeltaT() << std::endl;
                 break;
+            case 'y':
+                parameter = Input::trim(command.substr(2));
+                _programParameters->setEpsilon(std::__cxx11::stod(parameter));
+                std::cout << "MolSim Group G > Epsilon was set to " << _programParameters->getEpsilon() << std::endl;
+                break;
+            case 's':
+                parameter = Input::trim(command.substr(2));
+                _programParameters->setSigma(std::__cxx11::stod(parameter));
+                std::cout << "MolSim Group G > Sigma was set to " << _programParameters->getSigma() << std::endl;
+                break;
             case 'i':
             {
                 int num_particles = _programParameters->getParticleContainer()->size();
@@ -79,6 +89,8 @@ const void ConsoleMenu::openMenu()
                 }
                 std::cout << "MolSim Group G > Current end time: " << _programParameters->getEndTime() << std::endl;
                 std::cout << "MolSim Group G > Current delta time: " << _programParameters->getDeltaT() << std::endl;
+                std::cout << "MolSim Group G > Current epsilon: " << _programParameters->getEpsilon() << std::endl;
+                std::cout << "MolSim Group G > Current sigma: " << _programParameters->getSigma() << std::endl;
                 std::string mode = _programParameters->getBenchmarkIterations() == 0 ? "Simulation" : "Benchmark";
                 std::cout << "MolSim Group G > Current mode: " << mode << std::endl;
             }
@@ -135,12 +147,12 @@ const void ConsoleMenu::openMenu()
 
 const bool ConsoleMenu::verifyCommand(std::string command) const
 {
-    std::set<char> commands = {'f', 'c', 'e', 'd', 'x', 'r', 'h', 'q', 'i'};
+    std::set<char> commands = {'f', 'c', 'e', 'd', 'x', 'r', 'h', 'q', 'i', 'y', 's'};
     if (command.length() < 2 || command[0] != '-' || commands.count(command[1]) == 0)
     {
         return false;
     }
-    std::set<char> commandsWithParameters = {'f', 't', 'd'};
+    std::set<char> commandsWithParameters = {'f', 't', 'd', 'y', 's'};
     bool parameterTest;
     if (commandsWithParameters.count(command[1]) != 0)
     {
@@ -156,15 +168,7 @@ const bool ConsoleMenu::verifyCommand(std::string command) const
             }
         }
         break;
-        case 't':
-            parameterTest = Input::isDouble(Input::trim(command.substr(2)));
-            if (!parameterTest)
-            {
-                std::cout << "MolSim Group G > Error: Parameter is not a double" << std::endl;
-                return false;
-            }
-            break;
-        case 'd':
+        case 't': case 'd': case 'y': case 's':
             parameterTest = Input::isDouble(Input::trim(command.substr(2)));
             if (!parameterTest)
             {
@@ -184,6 +188,8 @@ const void ConsoleMenu::printHelpMenu() const
     printf("MolSim Group G > -f <filename> .......... The path to an input file. If not specified no particles are generated\n");
     printf("MolSim Group G > -e <end_time> .......... The end time of the simulation. If not specified, 100 is used\n");
     printf("MolSim Group G > -d <delta_t> ........... The size of the time steps in the simulation. If not specified 0.014 is used\n");
+    printf("MolSim Group G > -y <epsilon> ........... The epsilon value for calculation of the Lennard-Jones potential. If not specified 5 is used\n");
+    printf("MolSim Group G > -s <sigma> ............. The sigma value for calculation of the Lennard-Jones potential. If not specified 1 is used\n");
     printf("MolSim Group G > -i ..................... Displays the currently set values in the simulation\n");
     printf("MolSim Group G > -x ..................... Deletes all particles from the simulation\n");
     printf("MolSim Group G > -r ..................... Run the program with the currently set values\n");
