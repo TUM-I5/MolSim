81c2ee427ab4e1712f4e0eec7c217cb6eb91b412
AlexHocks <alex.hocks@tum.de>
2022-12-02 21:51:27 +0100

    normal version fixed

--- src/sim/physics/force/FLennardJones.cpp
+++ src/sim/physics/force/FLennardJones.cpp
@@ -6,13 +6,48 @@
 
 namespace sim::physics::force {
     void FLennardJones::operator()() {
-        //set all current forces on all particles to 0
-        particleContainer.forAllParticles([&](Particle &p) {
-            p.setOldF(p.getF());
-            p.setF({0., 0., 0.});
-        });
+        double sigma = this->sigma;
+        double epsilon = this->epsilon;
+        particleContainer.runOnData([sigma, epsilon](std::vector<double> &force,
+                                       std::vector<double> &oldForce,
+                                       std::vector<double> &x,
+                                       std::vector<double> &v,
+                                       std::vector<double> &m,
+                                       std::vector<int> &type,
+                                       unsigned long count){
+            for (unsigned long index = 0; index < count; index++) {
+                oldForce[index*3 + 0] = force[index*3 + 0];
+                oldForce[index*3 + 1] = force[index*3 + 1];
+                oldForce[index*3 + 2] = force[index*3 + 2];
+                force[index*3 + 0] = 0;
+                force[index*3 + 1] = 0;
+                force[index*3 + 2] = 0;
+            }
 
-        particleContainer.forAllPairs(pairFun);
+            double sigma6 = sigma * sigma * sigma * sigma * sigma * sigma;
+            double l2NInvSquare, fac0, l2NInvPow6, fac1_sum1, fac1, d0, d1, d2;
+            double* f = force.data();
+
+            for(unsigned long indexI = 0; indexI < count; indexI++){
+                for(unsigned long indexJ = indexI + 1; indexJ < count; indexJ++) {
+                    d0 = x[indexI*3 + 0] - x[indexJ*3 + 0];
+                    d1 = x[indexI*3 + 1] - x[indexJ*3 + 1];
+                    d2 = x[indexI*3 + 2] - x[indexJ*3 + 2];
+                    l2NInvSquare = 1 / (d0*d0 + d1*d1 + d2*d2);
+                    fac0 = 24 * epsilon * l2NInvSquare;
+                    l2NInvPow6 = l2NInvSquare * l2NInvSquare * l2NInvSquare;
+                    fac1_sum1 = sigma6 * l2NInvPow6;
+                    fac1 = (fac1_sum1) - 2 * (fac1_sum1 * fac1_sum1);
+
+                    f[indexI*3 + 0] += (-1) * fac0 * fac1 * d0;
+                    f[indexI*3 + 1] += (-1) * fac0 * fac1 * d1;
+                    f[indexI*3 + 2] += (-1) * fac0 * fac1 * d2;
+                    f[indexJ*3 + 0] += fac0 * fac1 * d0;
+                    f[indexJ*3 + 1] += fac0 * fac1 * d1;
+                    f[indexJ*3 + 2] += fac0 * fac1 * d2;
+                }
+            }
+        });
     }
 
 
