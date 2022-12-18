468d28a764f0b94e520f8167d4a5f164494f8cf8
riemenschneider <johannes.riemenschneider@tum.de>
2022-12-18 20:47:33 +0100

    Thermostat makes more sense like this

--- src/sim/physics/thermostat/Thermostat.h
+++ src/sim/physics/thermostat/Thermostat.h
@@ -29,25 +29,45 @@ private:
 
 
 public:
+    /**
+     * Initializes Thermostat sets the current Temperature to TInit
+     * In case the TInit is already surpassed from the user input nothing happens
+     * If no particle is initialized with "ordered" movement the current temperature gets set to TInit exactely
+     * If particles have "ordered" movement already every Body gets set to (approximately) TInit in their respective system
+     * Therefore in this case THE CURRENT TEMPERATURE IS UNEQUAL TO TInit!
+     *
+     * This might be confusing but hopefully as close as possible to the intended behaviour.
+     */
     explicit Thermostat(ParticleContainer& particleContainer, double T_t = default_t_target, unsigned int cT = default_n_term, unsigned int dimensions = default_dims, double dT = default_delta_temp, double TInit = default_t_init, bool thermoEnable = default_therm):
         pc(particleContainer), countThreshold(cT), dims(dimensions) {
         if(thermoEnable) {
-            auto TempToCreate{TInit - computeCurrentTemp()};
-            if (TempToCreate != 0.) { initializeBrownTemp(TempToCreate); }
+            auto curTemp{computeCurrentTemp()};
+            if(curTemp > TInit){
+                io::output::loggers::general->info("Initial temperature was lower than temperature given by user inputs. Therefore the no additional temperature got added");
+                return;
+            }else if(curTemp != 0) {
+                //in this case temp does NOT get normalized
+                io::output::loggers::general->info("TInit might not reflect the current Temperature since the temp gets initialized in the coordinate system of each respective particle");
+                initializeBrownTemp(TInit);
+            }else{
+                //in this case temp DOES get normalized
+                initializeBrownTemp(TInit);
 
-            //normalize towards our intended temperature:
-            //save real input parameters
-            auto realDeltaTemp{dT};
-            auto realTtarget{T_t};
+                //normalize towards our intended temperature:
+                //save real input parameters
+                auto realDeltaTemp{dT};
+                auto realTtarget{T_t};
 
-            //abuse getCooking to normalize to intended TInit
-            this->Ttarget = TInit;
-            this->deltaTemp = std::numeric_limits<double>::infinity();
-            this->getCooking();
+                //abuse getCooking to normalize to intended TInit
+                this->Ttarget = TInit;
+                this->deltaTemp = std::numeric_limits<double>::infinity();
+                this->getCooking();
 
-            //set values to their intended parameters
-            deltaTemp = realDeltaTemp;
-            Ttarget = realTtarget;
+                //set values to their intended parameters
+                deltaTemp = realDeltaTemp;
+                Ttarget = realTtarget;
+            }
+
         }
     }
 
@@ -55,6 +75,10 @@ public:
 
     void setParticleContainer(ParticleContainer& pc);
 
+    /**
+     * computes the current temperature relative to the Coordinate System of the Simulation
+     * @return
+     */
     double computeCurrentTemp();
 
     /**
