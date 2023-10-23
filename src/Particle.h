e78e3f9cf233be5fd78e432c0ca5e3fe0521fda4
Tobias <tobi.eppacher@gmail.com>
2023-10-23 17:13:54 +0200

    A bit of refactoring and command line input with boost_program_options

--- src/Particle.h
+++ src/Particle.h
@@ -11,75 +11,73 @@
 #include <string>
 
 class Particle {
+   private:
+    /**
+     * Position of the particle
+     */
+    std::array<double, 3> x;
 
-private:
-  /**
-   * Position of the particle
-   */
-  std::array<double, 3> x;
+    /**
+     * Velocity of the particle
+     */
+    std::array<double, 3> v;
 
-  /**
-   * Velocity of the particle
-   */
-  std::array<double, 3> v;
+    /**
+     * Force effective on this particle
+     */
+    std::array<double, 3> f;
 
-  /**
-   * Force effective on this particle
-   */
-  std::array<double, 3> f;
+    /**
+     * Force which was effective on this particle
+     */
+    std::array<double, 3> old_f;
 
-  /**
-   * Force which was effective on this particle
-   */
-  std::array<double, 3> old_f;
+    /**
+     * Mass of this particle
+     */
+    double m;
 
-  /**
-   * Mass of this particle
-   */
-  double m;
+    /**
+     * Type of the particle. Use it for whatever you want (e.g. to separate
+     * molecules belonging to different bodies, matters, and so on)
+     */
+    int type;
 
-  /**
-   * Type of the particle. Use it for whatever you want (e.g. to separate
-   * molecules belonging to different bodies, matters, and so on)
-   */
-  int type;
+   public:
+    explicit Particle(int type = 0);
 
-public:
-  explicit Particle(int type = 0);
+    Particle(const Particle &other);
 
-  Particle(const Particle &other);
+    Particle(
+        // for visualization, we need always 3 coordinates
+        // -> in case of 2d, we use only the first and the second
+        std::array<double, 3> x_arg, std::array<double, 3> v_arg, double m_arg, int type = 0);
 
-  Particle(
-      // for visualization, we need always 3 coordinates
-      // -> in case of 2d, we use only the first and the second
-      std::array<double, 3> x_arg, std::array<double, 3> v_arg, double m_arg,
-      int type = 0);
+    virtual ~Particle();
 
-  virtual ~Particle();
+    void setX(const std::array<double, 3> &x);
 
-  void setX(const std::array<double, 3> &x);
+    void setV(const std::array<double, 3> &v);
 
-  void setV(const std::array<double, 3> &v);
+    void setF(const std::array<double, 3> &f);
 
-  void setF(const std::array<double, 3> &f);
+    void setOldF(const std::array<double, 3> &oldF);
 
-  void setOldF(const std::array<double, 3> &oldF);
+    const std::array<double, 3> &getX() const;
 
-  const std::array<double, 3> &getX() const;
+    const std::array<double, 3> &getV() const;
 
-  const std::array<double, 3> &getV() const;
+    const std::array<double, 3> &getF() const;
 
-  const std::array<double, 3> &getF() const;
+    const std::array<double, 3> &getOldF() const;
 
-  const std::array<double, 3> &getOldF() const;
+    double getM() const;
 
-  double getM() const;
+    int getType() const;
 
-  int getType() const;
+    bool operator==(Particle &other);
 
-  bool operator==(Particle &other);
-
-  std::string toString() const;
+    std::string toString() const;
 };
 
 std::ostream &operator<<(std::ostream &stream, Particle &p);
--- src/outputWriter/VTKWriter.h
+++ src/outputWriter/VTKWriter.h
@@ -7,10 +7,10 @@
 
 #pragma once
 
+#include <list>
+
 #include "Particle.h"
 #include "outputWriter/vtk-unstructured.h"
-
-#include <list>
 
 namespace outputWriter {
 
@@ -19,35 +19,34 @@ namespace outputWriter {
  * particles.
  */
 class VTKWriter {
+   public:
+    VTKWriter();
 
-public:
-  VTKWriter();
+    virtual ~VTKWriter();
 
-  virtual ~VTKWriter();
+    /**
+     * set up internal data structures and prepare to plot a particle.
+     */
+    void initializeOutput(int numParticles);
 
-  /**
-   * set up internal data structures and prepare to plot a particle.
-   */
-  void initializeOutput(int numParticles);
+    /**
+     * plot type, mass, position, velocity and force of a particle.
+     *
+     * @note: initializeOutput() must have been called before.
+     */
+    void plotParticle(Particle &p);
 
-  /**
-   * plot type, mass, position, velocity and force of a particle.
-   *
-   * @note: initializeOutput() must have been called before.
-   */
-  void plotParticle(Particle &p);
+    /**
+     * writes the final output file.
+     *
+     * @param filename the base name of the file to be written.
+     * @param iteration the number of the current iteration,
+     *        which is used to generate an unique filename
+     */
+    void writeFile(const std::string &filename, int iteration);
 
-  /**
-   * writes the final output file.
-   *
-   * @param filename the base name of the file to be written.
-   * @param iteration the number of the current iteration,
-   *        which is used to generate an unique filename
-   */
-  void writeFile(const std::string &filename, int iteration);
-
-private:
-  VTKFile_t *vtkFile;
+   private:
+    VTKFile_t *vtkFile;
 };
 
-} // namespace outputWriter
+}  // namespace outputWriter
