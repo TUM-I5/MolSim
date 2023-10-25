2bac2683e2dd0897a1203d491a1d16b8cdc6931c
David <d.kratz@tum.de>
2023-10-25 22:01:23 +0200

    bugfixes of doxygen and CMake and doxygen option/target in correct file

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -5,8 +5,9 @@ option(DOXY_DOC "Documentation" ON)
 
 if(DOXY_DOC)
     find_package(Doxygen)
-    # ./Doxyfile, because the working directory is set to PSEMolDyn_GroupB
-    #this will also guarantee that the documentation is within the root folder
+    # ./Doxyfile, because the working directory/ directory in which doxygen documentation
+    # is build is set to PSEMolDyn_GroupB.
+    # this will also guarantee that the documentation is within the root folder
     add_custom_target(doc_doxygen
             COMMAND ${DOXYGEN_EXECUTABLE} ./Doxyfile
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
