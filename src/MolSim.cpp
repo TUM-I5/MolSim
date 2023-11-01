e16b1b653cbf1b44eff342c374573d97af7192ea
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-01 15:13:04 +0100

    Add program options using Boost.program_options

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -1,138 +1,88 @@
 #include "FileReader.h"
 #include "outputWriter/VTKWriter.h"
 #include "utils/ArrayUtils.h"
-#include "ParticleContainer.h"
+#include "models/ParticleContainer.h"
 
 #include <iostream>
 #include <list>
 #include <cmath>
+#include <boost/program_options.hpp>
 
-/**** forward declaration of the calculation functions ****/
+#include "Simulation.h"
+#include "models/Model.h"
 
-/**
- * calculate the force for all particles
- */
-void calculateF();
+namespace po = boost::program_options;
 
-/**
- * calculate the position for all particles
- */
-void calculateX();
+int main(int argc, char *argv[]) {
+    std::string in;
+    std::string out = "output";
+    double end_time = 1000;
+    double delta_t = 0.014;
+    int video_duration = 60;
+    int fps = 24;
 
-/**
- * calculate the position for all particles
- */
-void calculateV();
+    try {
+        po::options_description desc("MolSim program options");
 
-/**
- * plot the particles to a xyz-file
- */
-void plotParticles(int iteration);
+        desc.add_options()
+            ("help", "Information about MolSim")
+            ("in", po::value<std::string>(), "Provide input file path")
+            ("out", po::value<std::string>(), "Provide output file path")
+            ("video-duration", po::value<int>(), "Video duration in seconds")
+            ("fps", po::value<int>(), "Frames per second")
+            ("end-time", po::value<double>(), "End time")
+            ("delta-t", po::value<double>(), "Time delta for each iteration")
+        ;
 
-constexpr double start_time = 0;
-double end_time; // 1000
-double delta_t; // 0.014
+        po::positional_options_description p;
+        p.add("in", 1);
 
-ParticleContainer particles;
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+        po::notify(vm);
 
-int main(int argc, char *argsv[]) {
-
-    std::cout << "Hello from MolSim for PSE!" << std::endl;
-    if (argc != 4) {
-        std::cout << "Erroneous programme call! " << '\n';
-        std::cout << "./MolSim <input filename> <end time> <time delta>" << std::endl;
-        return 1;
-    }
-
-    char *end;
-    end_time = std::strtod(argsv[2], &end);
-
-    if (*end) {
-        std::cout << "Invalid end time: " << argsv[2] << std::endl;
-        return 1;
-    }
-
-    delta_t = std::strtod(argsv[3], &end);
-
-    if (*end) {
-        std::cout << "Invalid time delta: " << argsv[3] << std::endl;
-        return 1;
-    }
-
-
-    FileReader fileReader;
-    fileReader.readFile(particles, argsv[1]);
-
-    double current_time = start_time;
-
-    int iteration = 0;
-
-    // for this loop, we assume: current x, current f and current v are known
-    while (current_time < end_time) {
-        // calculate new x
-        calculateX();
-        // calculate new f
-        calculateF();
-        // calculate new v
-        calculateV();
-
-        iteration++;
-        if (iteration % 10 == 0) {
-            plotParticles(iteration);
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 1;
         }
-        std::cout << "Iteration " << iteration << " finished." << std::endl;
 
-        current_time += delta_t;
+        if (vm.count("in")) {
+            in = vm["in"].as<std::string>();
+        }
+
+        if (vm.count("out")) {
+            out = vm["out"].as<std::string>();
+        }
+
+        if (vm.count("video-duration")) {
+            video_duration = vm["video-duration"].as<int>();
+        }
+
+        if (vm.count("fps")) {
+            fps = vm["fps"].as<int>();
+        }
+
+        if (vm.count("end-time")) {
+            end_time = vm["end-time"].as<double>();
+        }
+
+        if (vm.count("delta-t")) {
+            delta_t = vm["delta-t"].as<double>();
+        }
+    } catch (std::exception &e) {
+        return 1;
     }
 
-    std::cout << "output written. Terminating..." << std::endl;
-    return 0;
-}
+    std::cout << "Hello from MolSim for PSE!\n" << std::endl;
+    if (argc < 2) {
+        std::cout << "Erroneous programme call! " << '\n';
+        std::cout << "./MolSim <input filename>\n" << "./MolSim --help" << std::endl;
+        return 1;
+    }
 
+    Simulation simulation{Model::basicModel(delta_t), end_time, delta_t, video_duration, fps, in, out};
 
-void calculateF() {
-    particles.applyToAll([](Particle &p) {
-        p.updateF(std::array<double, 3> {0., 0., 0.});
-    });
+    std::cout << "\nStarting simulation...\n" << simulation << std::endl;
 
-    particles.applyToAllPairs([](Particle &p1, Particle &p2) {
-        auto nextForce =
-                p1.getM() * p2.getM() / std::pow(p2.distanceTo(p1), 3) * p1.diffTo(p2);
-
-        p1.setF(p1.getF() + nextForce);
-    });
-}
-
-void calculateX() {
-    particles.applyToAll([](Particle &p) {
-        auto x =
-                p.getX() +
-                delta_t * p.getV() +
-                (delta_t * delta_t / (2 * p.getM())) * p.getF();
-
-        p.setX(x);
-    });
-}
-
-void calculateV() {
-    particles.applyToAll([](Particle &p) {
-        auto v =
-                p.getV() +
-                (delta_t / (2 * p.getM())) * (p.getOldF() + p.getF());
-
-        p.setV(v);
-    });
-}
-
-void plotParticles(int iteration) {
-
-    std::string out_name("output/MD_vtk");
-
-    outputWriter::VTKWriter writer;
-
-    writer.initializeOutput(particles.count());
-
-    particles.plot(writer);
-
-    writer.writeFile(out_name, iteration);
+    simulation.run();
 }
