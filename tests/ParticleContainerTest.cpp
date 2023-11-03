6ec7b7b6b88c836005b88a4dd212cd434136275c
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-03 19:14:32 +0100

    Create tests folder, and first tests

---
+++ tests/ParticleContainerTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Alp Kaan Aksu on 03.11.23.
+//
+
+#include <gtest/gtest.h>
+
+TEST(ParticleContainerTest, TestApplyToAll) {
+    ASSERT_STREQ("hi", "hi");
+}
+
+TEST(ParticleContainerTest, TestApplyToAll2) {
+    ASSERT_STREQ("no", "no");
+}
