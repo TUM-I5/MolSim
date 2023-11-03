c3aa6649f5e3c5ce74fcd4dea2176cf56db80ed3
Markus Vrancea <ge54jad@tum.de>
2023-11-03 14:52:40 +0100

    Doxygen Cmake File created. Updated .gitignore File to ignore the doxygen Documentation

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -3,3 +3,26 @@
 # TODO: create CMake build option for the target.
 
 # TODO: Add a custom target for building the documentation.
+
+#Option to enable/disable Doxygen
+option(BUILD_DOCUMENTATION "Generate Doxygen documentation" ON)
+
+if(BUILD_DOCUMENTATION)
+    find_package(Doxygen)
+
+    if(DOXYGEN_FOUND)
+
+        #Add custom target
+        add_custom_target(doc_doxygen
+                COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                COMMENT "Generating Doxygen documentation"
+        )
+
+        #Exclude from default build
+        set_target_properties(doc_doxygen PROPERTIES EXCLUDE_FROM_ALL TRUE)
+
+    else()
+        message(WARNING "Doxygen not found. Unable to generate documentation.")
+    endif()
+endif()
