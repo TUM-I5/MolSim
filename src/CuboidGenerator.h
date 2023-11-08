746dbb7614178fc4ed64e3ad629ed32a1619d9e3
Grazv <grazvydas.kuras@gmail.com>
2023-11-08 15:08:54 +0100

    cleaned up CuboidGenerator code

--- src/CuboidGenerator.h
+++ src/CuboidGenerator.h
@@ -2,7 +2,28 @@
 
 #include "ParticleContainer.h"
 #include "FileReader.h"
+#include "utils/MaxwellBoltzmannDistribution.h"
 
-namespace CuboidGenerator {
-    void generateCuboid(FileReader::CuboidData& cuboidData, ParticleContainer& particleContainer);
+void generateCuboid(FileReader::CuboidData& cuboid, ParticleContainer& particleContainer) {
+    for(int z = 0; z < cuboid.N3; z++) {
+
+        for (int y = 0; y < cuboid.N2; y++) {
+
+            for (int x = 0; x < cuboid.N1; x++) {
+                std::array<double, 3> cords(cuboid.x);
+                std::array<double, 3> vel(cuboid.v);
+                std::array<double, 3> dist(maxwellBoltzmannDistributedVelocity(cuboid.avg_v, 3));
+
+                cords[0] += x * cuboid.h;
+                cords[1] += y * cuboid.h;
+                cords[2] += z * cuboid.h;
+
+                vel[0] += dist[0];
+                vel[1] += dist[1];
+                vel[2] += dist[2];
+
+                particleContainer.addParticle(cords, vel, cuboid.m);
+            }
+        }
+    }
 }
