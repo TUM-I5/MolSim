f105d7a280c7c069b4a892e032b7064ebc5358a8
Grazv <grazvydas.kuras@gmail.com>
2023-11-08 14:48:11 +0100

    added maxwell boltzmann distribution to velocity

--- src/CuboidGenerator.cpp
+++ src/CuboidGenerator.cpp
@@ -1,4 +1,5 @@
 #include "CuboidGenerator.h"
+#include "utils/MaxwellBoltzmannDistribution.h"
 
 void CuboidGenerator::generateCuboid(FileReader::CuboidData& cuboid, ParticleContainer& particleContainer) {
     for(int z = 0; z < cuboid.N3; z++) {
@@ -7,12 +8,16 @@ void CuboidGenerator::generateCuboid(FileReader::CuboidData& cuboid, ParticleCon
 
             for (int x = 0; x < cuboid.N1; x++) {
                 std::array<double, 3> cords(cuboid.x);
-                //todo calculate velocity
                 std::array<double, 3> vel(cuboid.v);
+                std::array<double, 3> dist(maxwellBoltzmannDistributedVelocity(cuboid.avg_v, 3));
 
                 cords[0] += x * cuboid.h;
                 cords[1] += y * cuboid.h;
                 cords[2] += z * cuboid.h;
+
+                vel[0] += dist[0];
+                vel[1] += dist[1];
+                vel[2] += dist[2];
 
                 particleContainer.addParticle(cords, vel, cuboid.m);
             }
