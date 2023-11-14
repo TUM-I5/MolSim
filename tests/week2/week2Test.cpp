ccd94f54c5cf5a3b86ee1d900843160fab73882a
David <d.kratz@tum.de>
2023-11-14 11:30:32 +0100

    gmock + removed unnecessary stuff

--- tests/week2/week2Test.cpp
+++ tests/week2/week2Test.cpp
@@ -1,12 +1,25 @@
 #include <gtest/gtest.h>
-#include "particleModel/ParticleContainer.h"
+#include <gmock/gmock.h>
 
+// Create a mock class
+class MockFoo {
+public:
+    MOCK_METHOD(int, getValue, (), (const));
+};
 
-/**
- * Test if the iterator works correctly by inserting three elements
- * and checking if the elements are returned in the correct order
-*/
-TEST(test_iterator,test_iterator1){
-    //empty Test
+// Create a test fixture
+class MockTest : public testing::Test {
+protected:
+    MockFoo foo;
+};
+
+// Test using the mock object
+TEST_F(MockTest, TestMockFoo) {
+    // Set an expectation on the mock object
+    EXPECT_CALL(foo, getValue())
+        .WillOnce(testing::Return(42));
+
+    // Use the mock object
+    int value = foo.getValue();
+    EXPECT_EQ(value, 42);
 }
-
