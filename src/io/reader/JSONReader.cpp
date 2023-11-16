f4bf4d931cca31c586a7e0cd9b3bd505a8c405a2
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-16 15:53:24 +0100

    Check if JSON file exists in JSONReader

--- src/io/reader/JSONReader.cpp
+++ src/io/reader/JSONReader.cpp
@@ -4,9 +4,19 @@
 
 #include "JSONReader.h"
 
+#include <boost/filesystem.hpp>
+#include "spdlog/spdlog.h"
+
 #include <fstream>
 
 json JSONReader::readFile(const std::string &filename) {
+    // check if file at filename exists
+
+    if (!boost::filesystem::exists(filename)) {
+        spdlog::error("File " + filename + " does not exist.");
+        exit(-1);
+    }
+
     std::ifstream ifs(filename);
     json j = json::parse(ifs);
 
