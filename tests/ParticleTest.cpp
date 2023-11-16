c404cec2b7d6bf21c9b98781f2ab40e2221a5485
feryalezgi <ge95guv@mytum.de>
2023-11-17 00:42:13 +0100

    ParticleTest.cpp logs for consistency

--- tests/ParticleTest.cpp
+++ tests/ParticleTest.cpp
@@ -3,6 +3,7 @@
 //
 #include <gtest/gtest.h>
 #include "../src/models/ParticleContainer.h"
+#include "spdlog/spdlog.h"
 
 
 class ParticleTest : public testing::Test {
@@ -18,15 +19,20 @@ protected:
 
 // Test case to calculate the difference between particle coordinates
 TEST_F(ParticleTest, TestDiffTo) {
+    spdlog::info("Starting TestDiffTo");
     std::array<double, 3> diff = particle1.diffTo(particle2);
 
     EXPECT_EQ(diff[0], particle2.getX()[0] - particle1.getX()[0]);
     EXPECT_EQ(diff[1], particle2.getX()[1] - particle1.getX()[1]);
     EXPECT_EQ(diff[2], particle2.getX()[2] - particle1.getX()[2]);
+    spdlog::info("TestDiffTo completed");
+
 }
 
 // Test case for distanceTo, by comparing it to the Euclidean distance formula
 TEST_F(ParticleTest, TestDistanceTo) {
+    spdlog::info("Starting TestDistanceTo");
+
     Particle particle1({1.0, 2.0, 3.0}, {0.0, 0.0, 0.0}, 1.0, 1);
     Particle particle2({1.0, 6.0, 3.0}, {0.0, 0.0, 0.0}, 1.0, 2);
 
@@ -36,4 +42,6 @@ TEST_F(ParticleTest, TestDistanceTo) {
     double expectedDistance = 4.0;  // Calculated using the Euclidean distance formula
     double tolerance = 1e-5;
     EXPECT_NEAR(distance, expectedDistance, tolerance) << "Distance between two particles is not as expected.";
+    spdlog::info("TestDistanceTo completed");
+
 }
