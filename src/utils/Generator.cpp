7e3d02bf49f311fbf65e02afd94d6fbbddcb52b0
feryalezgi <ge95guv@mytum.de>
2023-11-19 18:21:12 +0100

    Extended the particle generator to generate spheres: methods Generator::sphere and its helper addParticleIfInsideSphere

--- src/utils/Generator.cpp
+++ src/utils/Generator.cpp
@@ -3,8 +3,12 @@
 //
 
 #include "Generator.h"
+#include <cmath>
+#include <array>
 
-void Generator::cuboid(ParticleContainer &container, std::array<double, 3> position, std::array<double, 3> size, double meshWidth, std::array<double, 3> velocity, double mass, int typeId) {
+
+void Generator::cuboid(ParticleContainer &container, std::array<double, 3> position, std::array<double, 3> size,
+                       double meshWidth, std::array<double, 3> velocity, double mass, int typeId) {
     for (int x = 0; x < size[0]; x++) {
         for (int y = 0; y < size[1]; y++) {
             for (int z = 0; z < size[2]; z++) {
@@ -21,3 +25,38 @@ void Generator::cuboid(ParticleContainer &container, std::array<double, 3> posit
         }
     }
 }
+
+// Checks if a given position is inside the sphere defined by the parameters position
+void Generator::addParticleIfInsideSphere(ParticleContainer &container, std::array<double, 3> position, std::array<double, 3> velocity,
+                                          double mass, int typeId, std::array<double, 3> center, int radius, double meshWidth) {
+    // normalized distance between the particle and the center in each dimension
+    // squared to simplify comparison
+    double distanceSquared = std::pow((position[0] - center[0]) / meshWidth, 2) +
+                             std::pow((position[1] - center[1]) / meshWidth, 2) +
+                             std::pow((position[2] - center[2]) / meshWidth, 2);
+
+    if (distanceSquared <= std::pow(radius, 2)) {
+        container.add(Particle{position, velocity, mass, typeId});
+    }
+}
+
+// Iterate over a cubic area around the sphere with the given parameters and add a particle to container if it is inside the sphere boundaries
+void Generator::sphere(ParticleContainer &container, std::array<double, 3> center, int radius, double meshWidth,
+                       std::array<double, 3> velocity, double mass, int typeId) {
+
+    // distance from the center to the edge of the sphere
+    double dis = radius * meshWidth;
+    for (double x = center[0] - dis; x <= center[0] + dis; x += meshWidth) {
+        for (double y = center[1] - dis; y <= center[1] + dis; y += meshWidth) {
+            for (double z = center[2] - dis; z <= center[2] + dis; z += meshWidth) {
+                addParticleIfInsideSphere(container, {x, y, z}, velocity, mass, typeId, center, radius, meshWidth);
+            }
+        }
+    }
+}
+
+
+
+
+
+
--- src/utils/Generator.h
+++ src/utils/Generator.h
@@ -1,6 +1,6 @@
 //
 // Created by Alp Kaan Aksu on 04.11.23.
-//
+// Updated by Feryal Ezgi Aşkın on 19.11.23
 
 #pragma once
 
@@ -8,5 +8,16 @@
 #include "models/ParticleContainer.h"
 
 namespace Generator {
-    void cuboid(ParticleContainer &container, std::array<double, 3> position, std::array<double, 3> size, double meshWidth, std::array<double, 3> velocity, double mass, int typeId);
+    void
+    cuboid(ParticleContainer &container, std::array<double, 3> position, std::array<double, 3> size, double meshWidth,
+           std::array<double, 3> velocity, double mass, int typeId);
+
+    void sphere(ParticleContainer &container, std::array<double, 3> center, int radius, double meshWidth,
+                std::array<double, 3> velocity, double mass, int typeId);
+
+
+    void addParticleIfInsideSphere(ParticleContainer &container, std::array<double, 3> position,
+            std::array<double, 3> velocity, double mass, int typeId,
+            std::array<double, 3> center, int radius, double meshWidth);
+
 }
