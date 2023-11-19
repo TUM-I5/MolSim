37ba701c80d08d801159a1263b2145b10b74893b
feryalezgi <ge95guv@mytum.de>
2023-11-19 19:04:13 +0100

    New cuboid test and size fix

--- tests/GeneratorTest.cpp
+++ tests/GeneratorTest.cpp
@@ -2,9 +2,10 @@
 // Created by Feryal Ezgi on 16.11.2023.
 //
 #include <gtest/gtest.h>
+#include <random>
+#include <spdlog/spdlog.h>
 #include "../src/models/ParticleContainer.h"
 #include "../src/utils/Generator.h"
-#include <spdlog/spdlog.h>
 
 class GeneratorTest : public ::testing::Test {
 protected:
@@ -21,25 +22,48 @@ protected:
     ParticleContainer particleContainer;
 };
 
+
+
 //Simple test case to check if the correct amount of particles has been added to the container after generating a cuboid
-TEST_F(GeneratorTest, CuboidTest) {
+TEST_F(GeneratorTest, BasicCuboidTest) {
     spdlog::info("Starting CuboidTest");
 
-    std::array<double, 3> position = {0.0, 0.0, 0.0};
-    std::array<double, 3> size = {2, 2, 2};
-    double meshWidth = 1.0;
-    std::array<double, 3> velocity = {1.0, 1.0, 1.0};
-    double mass = 1.0;
-    int typeId = 1;
+    Generator::cuboid(particleContainer, {0.0, 0.0, 0.0}, {2, 2, 2}, 1.0, {1.0, 1.0, 1.0}, 1.0, 1);
 
-    Generator::cuboid(particleContainer, position, size, meshWidth, velocity, mass, typeId);
-
-    EXPECT_EQ(particleContainer.size(), size[0] * size[1] * size[2]);
+    EXPECT_EQ(particleContainer.size(), 2 * 2 * 2);
 
     cuboidParticles = particleContainer.getParticles();
     TeardownParticleContainer();
     ASSERT_TRUE(particleContainer.size() == 0);
 
     spdlog::info("CuboidTest completed");
+}
 
+// Test cuboid generator with randomized parameters with size comparison
+TEST_F(GeneratorTest, RandomizedCuboidTest) {
+    spdlog::info("Starting CuboidTest");
+
+    //Some randomized parameters
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> doubleDist(-100.0, 100.0);
+    std::uniform_real_distribution<double> meshDist(0, 1.3);
+    std::uniform_real_distribution<int> intDist(-100.0, 100.0);
+
+    std::array<double, 3> position = {doubleDist(gen), doubleDist(gen), doubleDist(gen)};
+    std::array<double, 3> velocity = {doubleDist(gen), doubleDist(gen), doubleDist(gen)};
+    std::array<int, 3> size = {intDist(gen), intDist(gen), intDist(gen)};
+    double meshWidth = doubleDist(gen);
+    double mass = doubleDist(gen);
+    int typeId = 1;
+
+    Generator::cuboid(particleContainer, position, size, meshWidth, velocity, mass, typeId);
+    EXPECT_EQ(particleContainer.size(), static_cast<size_t>(size[0] * size[1] * size[2]));
+
+    cuboidParticles = particleContainer.getParticles();
+    TeardownParticleContainer();
+    // Ensure correct cleanup
+    ASSERT_TRUE(particleContainer.size() == 0);
+
+    spdlog::info("CuboidTest completed");
 }
