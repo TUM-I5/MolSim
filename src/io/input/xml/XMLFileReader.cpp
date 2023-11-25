9c1de7667aca13f8ac59f035e100efada07d8f67
ge63cal <ge63cal@mytum.de>
2023-11-25 16:24:24 +0100

    tests

--- src/io/input/xml/XMLFileReader.cpp
+++ src/io/input/xml/XMLFileReader.cpp
@@ -15,6 +15,22 @@ SimulationParams XMLFileReader::readFile(const std::string& filepath, ParticleCo
         Logger::logger->info("End time: {}", config->end_time().get());
         Logger::logger->info("Delta t: {}", config->delta_t().get());
 
+        if(config->fps().get() < 0){
+            Logger::logger->error("FPS must be positive");
+            exit(-1);
+        }
+        if(config->video_length().get() < 0){
+            Logger::logger->error("Video length must be positive");
+            exit(-1);
+        }
+        if(config->end_time().get() < 0){
+            Logger::logger->error("End time must be positive");
+            exit(-1);
+        }
+        if(config->delta_t().get() < 0){
+            Logger::logger->error("Delta t must be positive");
+            exit(-1);
+        }
 
         const std::string& output_dir_path = "";
         const std::string& log_level = "";
@@ -38,12 +54,24 @@ SimulationParams XMLFileReader::readFile(const std::string& filepath, ParticleCo
             }
             std::array<int,3> grid_dimensions_int{static_cast<int>(grid_dimensions[0]), static_cast<int>(grid_dimensions[1]), static_cast<int>(grid_dimensions[2])};
             std::array<double, 3> initial_velocity{config->cuboid()[i].velocity().x(), config->cuboid()[i].velocity().y(), config->cuboid()[i].velocity().z()};
-            if(initial_velocity[0] < 0 || initial_velocity[1] < 0 || initial_velocity[2] < 0){
-                Logger::logger->error("Cuboid initial velocity must be positive");
+
+            Logger::logger->info("Position : {} {} {}", lower_left_front_corner[0], lower_left_front_corner[1], lower_left_front_corner[2]);
+            if(config->cuboid()[i].grid_spacing() < 0){
+                Logger::logger->error("Cuboid grid spacing must be positive");
                 exit(-1);
             }
-            Logger::logger->info("Position : {} {} {}", lower_left_front_corner[0], lower_left_front_corner[1], lower_left_front_corner[2]);
-
+            if(config->cuboid()[i].mass() < 0){
+                Logger::logger->error("Particle mass must be positive");
+                exit(-1);
+            }
+            if(config->cuboid()[i].type() < 0){
+                Logger::logger->error("Particle type must be positive");
+                exit(-1);
+            }
+            if(config->cuboid()[i].temperature() < 0){
+                Logger::logger->error("Particle temperature (as it should be written in ° Kelvin) must be positive");
+                exit(-1);
+            }
             auto spawner = CuboidSpawner(lower_left_front_corner, grid_dimensions_int, config->cuboid()[i].grid_spacing(), config->cuboid()[i].mass(), initial_velocity, config->cuboid()[i].type(), config->cuboid()[i].temperature());
             particle_container.reserve(particle_container.size() + static_cast<long>(grid_dimensions[0]) * grid_dimensions[1] * grid_dimensions[2]);
             spawner.spawnParticles(particle_container);
---
+++ tests/io/filereaders/TestXMLReader.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+
+#include "data/FileLoader.h"
+#include "io/input/xml/XMLFileReader.h"
+#include "particles/ParticleContainer.h"
+#include "utils/ArrayUtils.h"
+
+#define EXPECT_CONTAINS_POS_NEAR(list, point, tol) \
+    EXPECT_TRUE(std::find_if(list.begin(), list.end(), [&](auto& x) { return ArrayUtils::L2Norm(x - point) < tol; }) != list.end());
+
+/*
+ * Test if a XMLFileReader reads the correct data out of the files.
+ */
+//TEST(XMLFileReader, WrongInput1) {
+//    ParticleContainer particle_container;
+//    XMLFileReader file_reader;
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("wrong_input1.xml"), particle_container));
+//
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("wrong_input2.xml"), particle_container));
+//
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("wrong_input3.xml"), particle_container));
+//}
+//TEST(XMLFileReader, WrongInput2) {
+//    ParticleContainer particle_container;
+//    XMLFileReader file_reader;
+//
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("wrong_input2.xml"), particle_container));
+//
+//}
+//
+//TEST(XMLFileReader, WrongInput3) {
+//    ParticleContainer particle_container;
+//    XMLFileReader file_reader;
+//
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("wrong_input3.xml"), particle_container));
+//
+//}
+//TEST(XMLFileReader, Overflow) {
+//    ParticleContainer particle_container;
+//    XMLFileReader file_reader;
+//
+//    EXPECT_ANY_THROW(file_reader.readFile(FileLoader::get_test_file_path("overflow.xml"), particle_container));
+//
+//}
+TEST(XMLFileReader, CorrectReadingOfParticles) {
+    ParticleContainer particle_container;
+    XMLFileReader file_reader;
+    SimulationParams conf = file_reader.readFile(FileLoader::get_test_file_path("test_collision.xml"), particle_container);
+
+    double err = 1e-13;
+    EXPECT_EQ(conf.video_length, 10);
+    EXPECT_EQ(conf.fps, 24);
+    EXPECT_NEAR(conf.end_time, 100, err);
+    EXPECT_NEAR(conf.delta_t, 0.0014, err);
+
+    EXPECT_EQ(particle_container.size(), 10 * 2 * 4 + 3 * 3 * 3 + 1);
+
+    for (int i = 0; i < 80; i++){
+        EXPECT_NEAR(particle_container[i].getM(), 1, err);
+    }
+    for (int i = 80; i < 89; i++){
+        EXPECT_NEAR(particle_container[i].getM(), 1.5, err);
+        EXPECT_EQ(2, particle_container[i].getType());
+    }
+    EXPECT_NEAR(particle_container[89].getM(), 10, err);
+
+    EXPECT_NEAR(particle_container[0].getX()[0], 0, err);
+    EXPECT_NEAR(particle_container[0].getX()[1], 0, err);
+    EXPECT_NEAR(particle_container[0].getX()[2], 0, err);
+
+    EXPECT_NEAR(particle_container[80].getX()[0], 100, err);
+    EXPECT_NEAR(particle_container[80].getX()[1], 100, err);
+    EXPECT_NEAR(particle_container[80].getX()[2], 20, err);
+
+    EXPECT_NEAR(particle_container[89].getX()[0], 100, err);
+    EXPECT_NEAR(particle_container[89].getX()[1], 100, err);
+    EXPECT_NEAR(particle_container[89].getX()[2], 20, err);
+}
