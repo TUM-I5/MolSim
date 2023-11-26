1e5d09a7b94208ecc33d007d008c89a5e546f7a2
Grazv <grazvydas.kuras@gmail.com>
2023-11-26 16:39:52 +0100

    adding typedef dim_t

--- src/particleModel/storage/CellContainer.h
+++ src/particleModel/storage/CellContainer.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <unordered_set>
 
+typedef unsigned short dim_t;
+
 struct ParticleHashing {
     size_t operator()(const Particle& obj) const {
 
@@ -14,13 +16,17 @@ struct ParticleHashing {
 
 class CellContainer {
 public:
-    CellContainer(size_t domain_x, size_t domain_y, size_t domain_z, double r_cutoff, double cell_size);
+    CellContainer(dim_t domain_x, dim_t domain_y, dim_t domain_z, double r_cutoff, double cell_size);
 
     virtual ~CellContainer();
 
+    void setNextCell(std::array<dim_t, 3> &next_position);
+
+    void setNextPath(std::array<dim_t, 6> &start_and_pattern);
+
     void addParticle(std::array<double, 3> x_arg, std::array<double, 3> v_arg, double m_arg);
 
-    void allocateCell(std::array<double, 3> &x, std::array<unsigned short, 3> &cell_position);
+    void allocateCell(std::array<double, 3> &x, std::array<dim_t , 3> &cell_position);
 
     void plotParticles(outputWriter::VTKWriter &writer);
 
@@ -28,9 +34,13 @@ public:
 
     size_t size();
 
+    auto getParticles() {
+        return &particles;
+    }
+
 private:
     double cell_size;
-    std::array<size_t, 3> domain_dim;
+    std::array<dim_t, 3> domain_dim;
     std::array<double, 3> domain_borders;
     size_t comparing_depth = 1;
     size_t particle_amount = 0;
