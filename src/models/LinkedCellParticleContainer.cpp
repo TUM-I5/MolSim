87893372ee28d95ae56824d403abe11752c8e216
BerkeSaylan <berksay@yahoo.com>
2023-12-02 18:27:24 +0100

    reflection/outflow logic seperated for every boundary, issues fixed

--- src/models/LinkedCellParticleContainer.cpp
+++ src/models/LinkedCellParticleContainer.cpp
@@ -316,8 +316,12 @@ void LinkedCellParticleContainer::vectorReverseReflection(Particle& particle) {
     double zMin = -static_cast<double>(zSize) / 2.0;
 
 
-    while(particle.getX()[0] <= xMin || particle.getX()[0] >= xMax || particle.getX()[1] <= yMin ||
-            particle.getX()[1] >= yMax || particle.getX()[2] <= zMin || particle.getX()[2] >= zMax) {
+    while(((particle.getX()[0] <= xMin && boundaryBehaviorRight == BoundaryBehavior::Reflective) ||
+           (particle.getX()[0] >= xMax && boundaryBehaviorLeft == BoundaryBehavior::Reflective)) ||
+          ((particle.getX()[1] <= yMin && boundaryBehaviorTop == BoundaryBehavior::Reflective) ||
+           (particle.getX()[1] >= yMax && boundaryBehaviorBottom == BoundaryBehavior::Reflective)) ||
+          ((particle.getX()[2] <= zMin && boundaryBehaviorFront == BoundaryBehavior::Reflective) ||
+           (particle.getX()[2] >= zMax && boundaryBehaviorBack == BoundaryBehavior::Reflective))) {
 
         reflectIfNecessaryOnAxis(particle, particle.getX()[0], xMin, xMax, 0);
 
