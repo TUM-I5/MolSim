4a9ac849c05349158e612d05be471ad39063cea4
yuchen zhao <ge64niy@mytum.de>
2023-12-04 01:36:29 +0100

    SphereGeneration 2D and 3D; Comments for SphereGeneration; Test for SphereGeneration

--- src/inputHandling/SphereGeneration.h
+++ src/inputHandling/SphereGeneration.h
@@ -2,8 +2,40 @@
 
 #include "particleModel/storage/SimulationContainer.h"
 #include "FileReader.h"
-void addSpheres(SimulationContainer& particleContainer, std::list<FileReader::SphereData> spheres);
 
-void generateSpheresMethod1(FileReader::SphereData& sphere, SimulationContainer& particleContainer);
+/**
+ * @brief gets global dimension of the particles and iteratively generates spheres
+ *
+ * Dimension is determined based on the needed z value of the domain.
+ * If z_domain= 1 then it is a 2D sphere, otherwise it is 3D.
+ * Capacity for the generated particles will be calculated and reserved within the particleContainer.
+ * Calls generateSpheres(...) for every cuboid.
+ *
+ *
+ * @param particleContainer reference to add particles to
+ * @param spheres all the spheres data to iterate over
+ * @param dim dimension of all sphere particles
+ */
+void addSpheres(SimulationContainer& particleContainer, std::list<FileReader::SphereData> spheres, size_t dim);
 
-void generateSpheresMethod2(FileReader::SphereData& sphere, SimulationContainer& particleContainer);
+
+
+/**
+ * @brief creates all particles of the Sphere and adds them into the ParticleContainer
+ *
+ * The given R represents the maximum number of particles on the radius.
+ * Calculates the radius length by multiply R and the mesh width.
+ * Creates a square/cube according to the dimension with the length 2 radius length filled with particles.
+ * Then determine whether the distance of each particle to the spheres center is within the radius length
+ * adding a very small number, so that the particles on the surface of the sphere are also included.
+ * If so adds the particles velocity, position and mass to the particle container.
+ * Maxwell boltzmann distribution is not applied here since we dont have a temperature as input.
+ *
+ * @param sphere data for the generation
+ * @param particleContainer reference to add the spheres particles to
+
+ */
+
+void generateSpheresMethod2D(FileReader::SphereData& sphere, SimulationContainer& particleContainer);
+
+void generateSpheresMethod3D(FileReader::SphereData& sphere, SimulationContainer& particleContainer);
--- tests/week3/week3_SphereGeneration.cpp
+++ tests/week3/week3_SphereGeneration.cpp
@@ -1,3 +1,30 @@
-//
-// Created by nana on 22.11.23.
-//
+#include <gtest/gtest.h>
+#include "inputHandling/FileReader.h"
+#include "inputHandling/SphereGeneration.h"
+#include "particleModel/storage/ParticleContainer.h"
+
+TEST (sphereGeneration,test_sphereGeneration){
+
+    FileReader::SphereData s1{{0, 0, 0}, {0, 0, 0}, 1, 2, 1.1225, 1, 5};
+
+    std::list<FileReader::SphereData> spheres{s1};
+
+    ParticleContainer particleContainer;
+
+    addSpheres(particleContainer, spheres, 2);
+
+   // check that the z dimention of the spheres is set to 0
+
+    for(int i = 0; i < particleContainer.size(); i++) {
+    ASSERT_EQ(particleContainer[i].getV().at(2), 0.0);
+    }
+
+    // check that a sphere with the radius of 2 has 13 particles inside
+    // as             *
+    //             *  *  *
+    //          *  *  *  *  *
+    //             *  *  *
+    //                *
+    ASSERT_EQ(particleContainer.size(), 13);
+
+}
